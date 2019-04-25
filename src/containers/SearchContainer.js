df7d031aa05cf10acf6726c8c487b2c5e7c7812e
chokoreetosan <mallepalliabhishek@gmail.com>
2019-04-25 10:38:31 -0700

    passes all tests

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,22 +2,19 @@ import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleVideoSearch from '../actions/search.js';
 
-var SearchContainer = () => {
 const mapStateToProps = (state)=>{
   return {
-    currentVideo: state.currentVideo,
-    videoList: state.videoList
+    currentVideo: state.video,
+    videoList: state.videos
   }
 
 }
 const mapDispatchToProps = (dispatch) =>{
   return {
-    handleInputChange: (query) => dispatch(handleVideoSearch(query))
+    handleSearchInputChange: (query) => dispatch(handleVideoSearch(query))
   };
 }
-
-return connect(mapStateToProps,mapDispatchToProps)(Search);
-};
+var SearchContainer = connect(mapStateToProps,mapDispatchToProps)(Search);
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
